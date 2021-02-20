674a101f2e4fd4c4960515e9d978c483970df862
Mosuhyeok <48712198+Mosuhyeok@users.noreply.github.com>
2021-02-20 15:13:03 +0900

    cube + enemy settings

---
+++ Assets/Works/수혁-enemy/Scripts/TestPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    void OnHitBullet(){
+        Debug.Log("적중");
+    }
+}
