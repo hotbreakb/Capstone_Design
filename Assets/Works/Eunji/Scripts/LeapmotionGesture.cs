bd603896c0ca9ee0bb91357e7dcda70467bc0af1
EUNJI SEONG <64337152+EUNJI1012@users.noreply.github.com>
2021-02-23 19:03:48 +0900

    isPlaying = true;

--- Assets/Works/Eunji/Scripts/LeapmotionGesture.cs
+++ Assets/Works/Eunji/Scripts/LeapmotionGesture.cs
@@ -108,7 +108,8 @@ public class LeapmotionGesture : MonoBehaviour
 #if UNITY_EDITOR
         // Application.Quit() does not work in the editor so
         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
-        UnityEditor.EditorApplication.isPlaying = false;
+
+        //UnityEditor.EditorApplication.isPlaying = false;
 #else
         Application.Quit();
 #endif
