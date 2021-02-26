80d9e1eea594e541ac967249212eec4fb93a4281
Helen1012 <areyhappy12@gmail.com>
2021-02-26 23:19:05 +0900

    makeTutorialScene

---
+++ Assets/Works/Eunji/Scripts/LoadScene/CheckLeapmotion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Leap;
+
+
+public class CheckLeapmotion : MonoBehaviour
+{
+    Controller leapmotion;
+
+    private void Start()
+    {
+        leapmotion = new Controller();
+
+        if (!leapmotion.IsConnected)
+        {
+            leapmotion = new Controller(1);
+        }
+    }
+
+    public void onClickLikeButton()
+    {
+        if (leapmotion.IsConnected)
+            Invoke("LoadNextScene", 2.0f);
+    }
+
+    private void LoadNextScene()
+    {
+        int index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(index + 1);
+    }
+}
