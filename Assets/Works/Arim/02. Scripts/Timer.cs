20edd304442cade79b415d73345249bc49b377b0
rrrrim <57060716+rrrrim@users.noreply.github.com>
2021-02-27 01:58:07 +0900

    Timerbar, labtop

---
+++ Assets/Works/Arim/02. Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeSpeed = 0.01f;
+    private float totalTime = 0f;
+    public Image progressBar;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        totalTime += Time.deltaTime;
+        progressBar.fillAmount = totalTime * timeSpeed;
+
+
+    }
+  
+}
