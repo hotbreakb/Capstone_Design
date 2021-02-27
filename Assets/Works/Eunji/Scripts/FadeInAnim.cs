1b1b3943099b5ffd0f6761a0571bf92710703f79
Helen1012 <areyhappy12@gmail.com>
2021-02-27 09:55:04 +0900

    make Tutorial Scene

---
+++ Assets/Works/Eunji/Scripts/FadeInAnim.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class FadeInAnim : MonoBehaviour
+{
+
+    float time = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 3f)
+        {
+            GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, time / 3);
+        }
+        else
+        {
+            time = 0;
+            this.gameObject.SetActive(false);
+        }
+        time += Time.deltaTime;
+
+    }
+
+    public void resetAnim()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
+        this.gameObject.SetActive(true);
+        time = 0;
+    }
+}
