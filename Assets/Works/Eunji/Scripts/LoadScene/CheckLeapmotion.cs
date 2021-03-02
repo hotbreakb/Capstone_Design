1da687b62a7dc11bfbc8a909cc3fba6470e34ac9
Helen1012 <areyhappy12@gmail.com>
2021-03-02 22:55:21 +0900

    make Tutorial

--- Assets/Works/Eunji/Scripts/LoadScene/CheckLeapmotion.cs
+++ Assets/Works/Eunji/Scripts/LoadScene/CheckLeapmotion.cs
@@ -2,15 +2,19 @@
 using UnityEngine.SceneManagement;
 using Leap;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CheckLeapmotion : MonoBehaviour
 {
     Controller leapmotion;
     public Text notConnected;
+    public UnityEngine.UI.Image circle;
 
     private void Start()
     {
         leapmotion = new Controller();
+        notConnected = GameObject.Find("NotConnected").GetComponent<Text>();
+        //circle = GameObject.Find("Circle").GetComponent<UnityEngine.UI.Image>();
 
         if (!leapmotion.IsConnected)
         {
@@ -18,10 +22,13 @@ public class CheckLeapmotion : MonoBehaviour
         }
 
         notConnected.gameObject.SetActive(false);
+        circle.gameObject.SetActive(false);
     }
 
     public void onClickLikeButton()
     {
+        circle.gameObject.SetActive(true);
+
         if (leapmotion.IsConnected)
             Invoke("LoadNextScene", 2.0f);
         else
