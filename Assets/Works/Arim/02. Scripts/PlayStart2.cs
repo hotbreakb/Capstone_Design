73bf030d8bdcd33207d2f913eb311106ea3e9ec5
rrrrim <57060716+rrrrim@users.noreply.github.com>
2021-03-04 21:24:43 +0900

    Playmode2
    script - PlayStart2
    prefab - PointLights, WayPointGroup
    
    생성

---
+++ Assets/Works/Arim/02. Scripts/PlayStart2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayStart2 : MonoBehaviour
+{
+    public float changeTime = 20.0f;
+    public GameObject Space;
+    //public Transform Tspace;
+    public List<Light> lights;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator ChangeScale()
+    {
+        Space.transform.localScale = new Vector3(10, 10, 10);
+        var group = GameObject.Find("PointLights");
+        if (group != null)
+        {
+            group.GetComponentsInChildren<Light>(lights);
+            lights.range
+        }
+    }
+}
