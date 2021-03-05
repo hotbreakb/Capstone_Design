cf6b8853aeb335e8f0e1c6501a51a4d9e3a90962
Helen1012 <areyhappy12@gmail.com>
2021-03-05 20:38:52 +0900

    Add Gaze in Cursor

---
+++ Assets/Works/Eunji/Scripts/HideCursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = false;                     //마우스 커서가 보이지 않게 함
+        Cursor.lockState = CursorLockMode.Locked;   //마우스 커서를 고정시킴
+    }
+}
