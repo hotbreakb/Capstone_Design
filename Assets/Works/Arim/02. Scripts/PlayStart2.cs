39b616ccd90fd737e49f7319b2bfe88b9b7cd862
rrrrim <57060716+rrrrim@users.noreply.github.com>
2021-03-06 10:15:50 +0900

    PlayStart script

--- Assets/Works/Arim/02. Scripts/PlayStart2.cs
+++ Assets/Works/Arim/02. Scripts/PlayStart2.cs
@@ -6,9 +6,17 @@ public class PlayStart2 : MonoBehaviour
 {
     public float changeTime = 20.0f;
     public GameObject Space;
+    public GameObject desk;
+    public GameObject labtop;
     //public Transform Tspace;
-    public List<Light> lights;
-    
+    public Light light1;
+    public Light light2;
+    public Light light3;
+    public Light light4;
+    public Light light5;
+    public Light light6;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +26,22 @@ public class PlayStart2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Changes();
     }
-    /*IEnumerator ChangeScale()
+    void Changes()
     {
         Space.transform.localScale = new Vector3(10, 10, 10);
-        var group = GameObject.Find("PointLights");
-        if (group != null)
-        {
-            group.GetComponentsInChildren<Light>(lights);
-            lights.range
-        }
-    }*/
+        ChangeLight(light1);
+        ChangeLight(light2);
+        ChangeLight(light3);
+        ChangeLight(light4);
+        ChangeLight(light5);
+        ChangeLight(light6);
+
+    }
+    void ChangeLight(Light light)
+    {
+        light.range = 90.0f;
+        light.intensity = 7.0f;
+    }
 }
