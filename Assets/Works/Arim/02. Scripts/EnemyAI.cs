cc6d3234ecbe9cb2fb049537a8b520cfb00558a5
rrrrim <57060716+rrrrim@users.noreply.github.com>
2021-03-10 23:17:47 +0900

    Revert "Merge pull request #81 from Helen1012/propsEffect"
    This reverts commit 72367e98cf13f9fee45a6d9084f59633bfb162e6, reversing
    changes made to e1a3643ffec62a3fa172d2c30303fc830cfeeef4.

---
+++ Assets/Works/Arim/02. Scripts/EnemyAI.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public enum State{
+        PATROL,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    public State state = State.PATROL;
+
+    private Transform playerTr;
+    private Transform enemyTr;
+
+    // Animator 컴포넌트를 저장할 변수
+    private Animator animator;
+
+
+    public float attackDist = 5.0f;
+    public float traceDist = 10.0f;
+
+
+
+
+    public bool isDie = false;
+
+    private WaitForSeconds ws;
+
+    // 이동을 제어하는 MoveAgent클래스 저장할 변수
+    private MoveAgent moveAgent;
+
+
+    private EnemyFire enemyFire;
+
+
+    // 애니메이터 컨트롤러에 정의한 파라미터 해시값 미리 추출
+    private readonly int hashMove = Animator.StringToHash("IsMove");
+    private readonly int hashSpeed = Animator.StringToHash("Speed");
+
+
+
+
+    private void Awake(){
+        var player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null) playerTr = player.GetComponent<Transform>();
+
+        enemyTr = GetComponent<Transform>();
+        moveAgent = GetComponent<MoveAgent>();
+        animator = GetComponent<Animator>();
+        enemyFire = GetComponent<EnemyFire>();
+        ws = new WaitForSeconds(0.3f);
+
+    }
+
+    private void OnEnable(){
+        StartCoroutine(CheckState());
+        StartCoroutine(Action());
+    }
+    IEnumerator Action(){
+        while(!isDie){
+            yield return ws;
+            // 상태에 따라 분기처리
+            switch(state){
+                case State.PATROL:
+                    // 총알발사정지
+                    enemyFire.isFire = false;
+                    moveAgent.patrolling = true;
+                    animator.SetBool(hashMove, true);
+                    break;
+
+                case State.TRACE:
+                    enemyFire.isFire = false;
+                    moveAgent.traceTarget = playerTr.position;
+                    animator.SetBool(hashMove, true);
+                    break;
+
+                case State.ATTACK:
+                    moveAgent.Stop();
+                    animator.SetBool(hashMove, false);
+
+                    if(enemyFire.isFire == false) enemyFire.isFire = true;
+                    break;
+
+                case State.DIE:
+                    moveAgent.Stop();
+                    break;
+            }
+        }
+    }
+    IEnumerator CheckState(){
+        while(!isDie){
+            if(state == State.DIE) yield break;
+
+            float dist = Vector3.Distance(playerTr.position, enemyTr.position);
+
+            if (dist <=attackDist){
+                state = State.ATTACK;
+            }
+            else if(dist <=traceDist){
+                state = State.TRACE;
+            }
+            else{
+                state = State.PATROL;
+            }
+            yield return ws;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat(hashSpeed, moveAgent.speed);
+    }
+}
