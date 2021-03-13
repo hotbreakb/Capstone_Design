5ad2663a863a1325f2c2b9922d59563894c71267
rrrrim <57060716+rrrrim@users.noreply.github.com>
2021-03-13 11:29:26 +0900

    Camera shake, Fires

--- Assets/Works/Arim/02. Scripts/PlayStart2.cs
+++ Assets/Works/Arim/02. Scripts/PlayStart2.cs
@@ -23,6 +23,7 @@ public class PlayStart2 : MonoBehaviour
 
     [Header("Bomb effect")]
     public GameObject Bombs;
+    public GameObject Fires;
 
     [Header("UI")]
     public GameObject UI;
@@ -36,29 +37,18 @@ public class PlayStart2 : MonoBehaviour
     public float fadeOutTime = 1.0f;
 
     [Header("Shake Camera")]
-    public Camera gunCamera;
-    Vector3 cameraPos;
-    [SerializeField] [Range(0.01f, 0.1f)] float shakeRange = 0.05f;
-    [SerializeField] [Range(0.1f, 2f)] float duration = 2f;
+    public GameObject Canvas;
+    public GameObject GunCamera;
 
 
     private void Start()
     {
         Background.enabled = true;
-        //Shake();
-        //StartCoroutine(FadeOut());
         StartCoroutine(delay());
-        cameraPos = gunCamera.transform.localPosition;
 
 
     }
-    private void Update()
-    {
-        //StartCoroutine(FadeOut());
-        //StartCoroutine(delay());
-        //StartCoroutine("FadeOut");
-        //Shake(shakeRange, duration);
-    }
+
 
     
     IEnumerator FadeOut()
@@ -76,48 +66,10 @@ public class PlayStart2 : MonoBehaviour
         }
         yield return new WaitForSeconds(0.3f);
 
-        //num = color.a;
-        //color.a += Time.deltaTime * 0.005f;
-        //Background.color = color;
+     
     }
 
-    /*void Shake()
-    {
-        cameraPos = gunCamera.transform.position;
-        InvokeRepeating("StartShake", 0f, 0.005f);
-        Invoke("StopShake", duration);
-    }
-
-    void StartShake()
-    {
-        float cameraPosX = Random.value * shakeRange * 2 - shakeRange;
-        float cameraPosY = Random.value * shakeRange * 2 - shakeRange;
-        Vector3 cameraPos = gunCamera.transform.position;
-        cameraPos.x += cameraPosX;
-        cameraPos.y += cameraPosY;
-        gunCamera.transform.position = cameraPos;
-    }
-
-    void StopShake()
-    {
-        CancelInvoke("StartShake");
-        gunCamera.transform.position = cameraPos;
-    }*/
-
-    /*public IEnumerator Shake(float _amount, float _duration)
-    {
-        float timer = 0;
-        while (timer <= _duration)
-        {
-            transform.localPosition = (Vector3)Random.insideUnitCircle * _amount + cameraPos;
-            timer += Time.deltaTime;
-            yield return null;
-        }
-        transform.localPosition = cameraPos;
-    }
-    */
-    // Update is called once per frame
-
+   
     void Changes()
     {
         Player.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
@@ -127,15 +79,18 @@ public class PlayStart2 : MonoBehaviour
         bulletCase.transform.localScale = new Vector3(0.11f, 0.11f, 0.11f);
         desk.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
         desk.transform.position = new Vector3(0.339f, -1.53f, desk.transform.position.z);
-        //labtop.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
         grenade.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+        
+
 
 
     }
     IEnumerator delay()
     {
+        yield return StartCoroutine(FireAct());
+        yield return StartCoroutine(BombAct());
         yield return StartCoroutine(FadeOut());
-        yield return StartCoroutine("BombAct");
+        yield return StartCoroutine("BombnotAct");
         yield return StartCoroutine("EyeblinkNotAct");
         yield return StartCoroutine("PropsAct");
         yield return StartCoroutine("Changes");
@@ -156,7 +111,20 @@ public class PlayStart2 : MonoBehaviour
         Props.SetActive(false);
         messProps.SetActive(true);
     }
-    void BombAct()
+    IEnumerator FireAct()
+    {
+        yield return new WaitForSeconds(4f);
+        Fires.SetActive(true);
+        
+    }
+    IEnumerator BombAct()
+    {
+        yield return new WaitForSeconds(1f);
+        Bombs.SetActive(true);
+        Fires.SetActive(false);
+        Canvas.SendMessage("Shake");
+    }
+    void BombnotAct()
     {
         Bombs.SetActive(false);
     }
