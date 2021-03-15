6da576ae412bd6395bbdfcb619791ddb5a61cb65
Mosuhyeok <48712198+Mosuhyeok@users.noreply.github.com>
2021-03-15 22:21:37 +0900

    ㅇ
    ㅇ

---
+++ Assets/Works/수혁-enemy/Scripts/SC2HpBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SC2HpBar : MonoBehaviour
+{
+
+    public int maxHp =100;
+     int curHp = 100;           // 현재 cpfur
+
+
+    public Image hpbar;
+
+    void awake(){
+        hpbar.rectTransform.localScale = new Vector3(1f,1f,1f);
+    }
+
+    
+    public void UpdateAfterReceiveAttack(){
+        curHp-=10;
+        if(curHp ==0){
+            hpbar.color = Color.clear;
+        }
+        hpbar.rectTransform.localScale = new Vector3((float)curHp/(float)maxHp,1f,1f);
+    }
+}
