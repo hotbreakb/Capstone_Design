f88cbe23a9054bfa54890cd325beeb0be7a79706
Mosuhyeok <48712198+Mosuhyeok@users.noreply.github.com>
2021-03-19 19:52:38 +0900

    공속조절

---
+++ Assets/Works/수혁-enemy/Scripts/EnemyMeleeAttack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMeleeAttack : MonoBehaviour
+{   
+    private Animator animator;
+
+    private Transform playerTr;
+
+    private Transform enemyTr;
+
+    private readonly int hashMeleeAttack = Animator.StringToHash("MeleeAttack");
+    private readonly int hashMeleeAttackIdx = Animator.StringToHash("MeleeAttackIdx");
+
+    private float nextAttack = 0.0f;
+    private readonly float AttackRate = 0.3f;
+
+    public bool isMeleeAttack = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTr = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        enemyTr = GetComponent<Transform>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMeleeAttack){
+            if(Time.time >= nextAttack){
+               Attack();
+                nextAttack = Time.time + AttackRate + Random.Range(0.0f,1.0f);  // 공속딜레이
+            }
+            
+        }
+    }
+
+    private void Attack(){
+        animator.SetTrigger(hashMeleeAttack);
+        animator.SetInteger(hashMeleeAttackIdx,(Random.Range(1,4)));
+    }
+
+}
