36a5eaf13e88f984cf018fdbfaabad72ca861bff
Helen1012 <areyhappy12@gmail.com>
2021-03-21 11:34:26 +0900

    Edit Title script

---
+++ Assets/Works/Eunji/Scripts/ImageFade.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ImageFade : MonoBehaviour
+{
+    public Image img;
+    public Image backgroundImg;
+
+    private void Start()
+    {
+        //backgroundImg.enabled = false;
+        StartCoroutine(FadeImage(true));
+        //backgroundImg.enabled = true;
+    }
+
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        
+        if (fadeAway)
+        {
+            // fade from transparent to opaque
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(2.0f);
+
+            // fade from opaque to transparent
+            // loop over 1 second backwards
+            for (float i = 1; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+
+            GameObject.Find("Canvas").GetComponent<CTTDemoManager>().TitlePlay();
+        }
+    }
+}
