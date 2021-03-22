6f35064572301b2ff3201d06aa16ba8840cf9275
rrrrim <57060716+rrrrim@users.noreply.github.com>
2021-03-22 20:26:30 +0900

    asset 추가

---
+++ Assets/Works/Arim/02. Scripts/TypingUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class TypingUI : MonoBehaviour
+{
+	Text txt;
+	string story;
+
+	void Awake()
+	{
+		txt = GetComponent<Text>();
+		story = txt.text;
+		txt.text = "";
+
+		// TODO: add optional delay when to start
+		StartCoroutine("PlayText");
+	}
+
+	IEnumerator PlayText()
+	{
+		foreach (char c in story)
+		{
+			txt.text += c;
+			yield return new WaitForSeconds(0.125f);
+		}
+	}
+}
