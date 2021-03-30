f32ed46d430d1f454c6184e19dedab1994772024
EUNJI SEONG <64337152+Helen1012@users.noreply.github.com>
2021-03-30 10:34:32 +0900

    Set the Start Scene

---
+++ Assets/Works/Eunji/Scripts/LoadScene/LoadSceneAutoinStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneAutoinStart : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<LoadSceneAuto>().LoadNextScene();  
+    }
+}
