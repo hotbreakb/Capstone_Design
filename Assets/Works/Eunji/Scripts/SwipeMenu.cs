3029c0e8ba53c8a53728632a904d9626af080855
EUNJI SEONG <64337152+Helen1012@users.noreply.github.com>
2021-04-01 21:19:58 +0900

    make swipe

--- Assets/Works/Eunji/Scripts/SwipeMenu.cs
+++ Assets/Works/Eunji/Scripts/SwipeMenu.cs
@@ -1,37 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+
 public class SwipeMenu : MonoBehaviour
 {
     public GameObject scrollbar;
-
-    public GameObject ArrawBtn;
     private float scroll_pos;
+    private float distance;
     private float[] pos;
 
-    void Update()
+    private int levelSize = 6;
+    private bool _isClicked = false;
+    void Start()
     {
-        pos = new float[transform.childCount];
-        float dis = 1f / (pos.Length - 1f);
+        pos = new float[levelSize];
+        distance = 1f / (pos.Length - 1f);
 
         for (int i = 0; i < pos.Length; i++)
         {
-            pos[i] = dis * i;
+            pos[i] = distance * i;
         }
+    }
 
-        if (Input.GetMouseButton(0))
-        { // if arrow btn is clicked, the scroll is moved.
-            scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+    void Update()
+    {
+        if (_isClicked)
+        {
+            // scroll_pos = scrollbar.GetComponent<Scrollbar>().value;
+            scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[levelSize - 1], 0.01f);
+            Invoke("TurnOff", 2f);
         }
         else
         {
-            for (int i = 0; i < pos.Length; i++)
-            {
-                if (scroll_pos < pos[i] + (dis / 2) && scroll_pos > pos[i] - (dis / 2))
-                {
-                    scrollbar.GetComponent<Scrollbar>().value = Mathf.Lerp(scrollbar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
-                }
-            }
+            // for (int i = 0; i < pos.Length; i++)
+            // {
+                // if (scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance / 2))
+                // {
+                    
+                // }
+            // }
         }
+        
+    }
+
+    public void IsClicked()
+    {
+        if (_isClicked == false) _isClicked = true;
+    }
+
+    private void TurnOff() {
+        _isClicked = false;
     }
 }
