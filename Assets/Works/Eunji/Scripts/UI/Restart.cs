64cd27e098e53192887c1765c49ba7fe4c121d79
EUNJI SEONG <64337152+Helen1012@users.noreply.github.com>
2021-04-02 11:54:39 +0900

    make start text

--- Assets/Works/Eunji/Scripts/UI/Restart.cs
+++ Assets/Works/Eunji/Scripts/UI/Restart.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Restart : MonoBehaviour
 {
     public GameObject GrayPanel;
+    public TextMeshProUGUI RestartText;
     public GameObject RestartBtn;
     public GameObject YesBtn;
     public GameObject NoBtn;
 
-    public void playShow(){
-        StartCoroutine(show());
+    public void playShow(bool _isStart){
+        StartCoroutine(show(_isStart));
     }
 
-    public IEnumerator show() {
+    public IEnumerator show(bool _isStart) {
+        if(_isStart) RestartText.text = "start?";
+
         yield return new WaitForSecondsRealtime(1.3f);
         GrayPanel.SetActive(true);
         RestartBtn.SetActive(true);
