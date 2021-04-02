dbeac5daf8ebd003201ef5c53a67e78a7da3b520
EUNJI SEONG <64337152+Helen1012@users.noreply.github.com>
2021-04-02 11:23:01 +0900

    make Btn

---
+++ Assets/Works/Eunji/Scripts/UI/Restart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    public GameObject GrayPanel;
+    public GameObject RestartBtn;
+    public GameObject YesBtn;
+    public GameObject NoBtn;
+
+    public void playShow(){
+        StartCoroutine(show());
+    }
+
+    public IEnumerator show() {
+        yield return new WaitForSecondsRealtime(1.3f);
+        GrayPanel.SetActive(true);
+        RestartBtn.SetActive(true);
+        YesBtn.SetActive(true);
+        NoBtn.SetActive(true);
+    }
+
+    public void hide() {
+        GrayPanel.SetActive(false);
+        RestartBtn.SetActive(false);
+        YesBtn.SetActive(false);
+        NoBtn.SetActive(false);
+    }
+}
